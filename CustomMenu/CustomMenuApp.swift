7ce090165c9d2872aef729bca561386d8ca20625
유인호 <yuiop1029@naver.com>
2024-11-26 11:33:20 +0900

    Initial Commit

---
+++ CustomMenu/CustomMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomMenuApp.swift
+//  CustomMenu
+//
+//  Created by A_Mcflurry on 11/26/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
